a0ef906ac8dd2dd332f77c8a7555dee293083dcc
Yuwetha <yuthiesdesigners10@gmail.com>
2020-10-30 02:17:56 +0530

    first commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var postMessageRoutes = require('./controllers/postMessageController')
 var app = express()
 app.use(bodyParser.json())
 app.use(cors({origin:'http://localhost:3000'}))
-app.listen(4000,()=>console.log('Server started at : 4000'))
+app.listen(5000,()=>console.log('Server started at : 4000'))
 
 
 app.use('/postMessages',postMessageRoutes)
